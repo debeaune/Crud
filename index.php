b5279ae8b2aede942efcefb51132517c92e86e68
marie-laure Debeaune <ml.debeaune@gmail.com>
2022-08-23 10:38:23 +0200

    update

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<?php   
+    // On inclut la connexion à la base de données
+    require_once('database.php');
+?>
+
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE-Edge">
+        <Title>Crud</title>
+        <meta name="viewport" content="width-device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="index.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <h1>
+                    <img src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png" alt="Wild Code School logo" />
+                    Les Argonautes
+                </h1>
+            </header>
+            <main>
+            <h2>Ajouter un(e) Argonaute</h2>
+            <br>
+            <form action="action.php" method="post" class="new-member-form">
+                <label for="name">Nom de l'Argonaute</label>
+                <br>
+                <input name="name" type="text" class="form-control" placeholder="Charalampos" />
+                <br>
+                <button type="submit" class="btn btn-primary" name="bouton_valider" value="valider">Envoyer</button>
+            </form>
+            <br>
+            <h2>Membres de l'équipage</h2>
+                <section class="member-list">
+                    <div class="member-item">
+                        <?php 
+                            if (isset($_POST['bouton_valider']))
+                            { 
+                                include("database.php");
+                                $sql="SELECT * FROM members WHERE 'name' ='".$_POST['name']."'";
+                                $resultat = mysql_query($sql) or die('Erreur SQL !<br />'.$sql.'<br />'.mysql_error());
+                                while ($data = mysql_fetch_array($resultat)) {
+                                echo $data['name'];
+                            }
+                            }
+                        ?>
+                    </div>
+                    </section>
+                </main>
+            <footer>
+                <p>Réalisé par Jason en Anthestérion de l'an 515 avant JC</p>
+            </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+    </body>
+</html>
